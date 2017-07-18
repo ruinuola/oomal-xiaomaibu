2e505a25baf1aa8b1b2296acfd9b14585bc5f8f9
ruinuola <419319655@qq.com>
2017-07-18 16:46:29 +0800

    修改webpack.production.config.js

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -34,7 +34,7 @@ module.exports = {
         { test: /\.less$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss!less') },
         { test: /\.css$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss') },
         { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000&name=img/[name].[ext]' },
-        { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000&name=fonts/[name].[ext]'}
+        { test:/\.(woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000&name=fonts/[name].[ext]'}
     ]
   },
   postcss: [
